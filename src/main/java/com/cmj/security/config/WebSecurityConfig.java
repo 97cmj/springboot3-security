85bf6897df2780404d926091fb907040a0b92792
DESKTOP-61QRH02\dungh <dunghasd@gmail.com>
2023-11-08 23:07:26 +0900

    Feat: 스프링 시큐리티 환경설정 Config 추가

---
+++ src/main/java/com/cmj/security/config/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.cmj.security.config;
+
+import com.cmj.security.service.UserDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig {
+
+    private final UserDetailService userDetailService;
+
+    //스프링 시큐리티 보안 필터 체인 무시 설정
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web -> web.ignoring()
+                .requestMatchers("/static/**", "/favicon.ico"));
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login", "/sign-up", "/user").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login")
+                        .invalidateHttpSession(true)
+                ).csrf(withDefaults()
+                ).httpBasic(withDefaults()
+                ).userDetailsService(userDetailService);
+        return http.build();
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
